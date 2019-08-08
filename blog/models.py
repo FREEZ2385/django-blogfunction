5704cc1cf095cba56d8d78fa3791d388cf0f300c
Freez <schlee9703@naver.com>
2019-08-08 14:47:35 +0900

    Create Write/Delete function

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,9 +8,12 @@ class Blog(models.Model):
     blog_subtitle = models.CharField(max_length = 100)
     blog_contents = models.TextField()
     blog_time = models.DateTimeField()
+    blog_num = models.AutoField(primary_key = True)
 
     def __str__(self):
         return self.blog_title
+
+
 
     class Meta:
         ordering = ['-blog_time']
